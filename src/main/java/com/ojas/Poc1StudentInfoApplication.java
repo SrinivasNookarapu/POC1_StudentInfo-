32eff74ed5245c4673370d0dddce8ba39182de7a
Srinivas Nookarapu <sn21002@corp.ojas-it.com>
2021-07-09 12:16:00 +0530

    added poc-1 to local repo

---
+++ src/main/java/com/ojas/Poc1StudentInfoApplication.java
@@ -0,0 +1,13 @@
+package com.ojas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poc1StudentInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poc1StudentInfoApplication.class, args);
+	}
+
+}
